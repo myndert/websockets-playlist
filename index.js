dcf7b901743be62de0b959e9bedd7da8a4ba52a0
Ricky Li <ricky_0321@hotmail.com>
2018-06-23 18:45:01 -0500

    added socket connection

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const express = require('express');
+var express = require('express');
+var socketio = require('socket.io');
 
 //app setup
-const app = express();
-const server  = app.listen(4000, function(){
+var app = express();
+var server  = app.listen(4000, function(){
   console.log('listening to request on port 4000');
 });
 
 //Static file
 app.use(express.static('public'));
+
+// socket setup
+var io = socketio(server);
+
+io.on('connection', (socket) => {
+  console.log('made socket connection', socket.id);
+});
